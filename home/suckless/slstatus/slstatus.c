0daca2fac0b9d558839e57db733972d1b7e05e8e
aditya-k2 <adityakurdunkar2@gmail.com>
2026-01-16 12:09:56 +0530

    slstatus: Clean up

--- home/suckless/slstatus/slstatus.c
+++ home/suckless/slstatus/slstatus.c
@@ -30,6 +30,29 @@ terminate(const int signo)
 		done = 1;
 }
 
+const char *
+run_command(const char *cmd)
+{
+	char *p;
+	FILE *fp;
+
+	if (!(fp = popen(cmd, "r"))) {
+		warn("popen '%s':", cmd);
+		return NULL;
+	}
+	p = fgets(buf, sizeof(buf) - 1, fp);
+	if (pclose(fp) < 0) {
+		warn("pclose '%s':", cmd);
+		return NULL;
+	}
+	if (!p)
+		return NULL;
+	if ((p = strrchr(buf, '\n')))
+		p[0] = '\0';
+
+	return buf[0] ? buf : NULL;
+}
+
 static void
 difftimespec(struct timespec *res, struct timespec *a, struct timespec *b)
 {
