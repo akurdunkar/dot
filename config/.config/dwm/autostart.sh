caf47d129b96b5372dfa9e52d31a84d5ce2e40cc
Aditya Kurdunkar <adityakurdunkar2@gmail.com>
2026-03-26 10:49:57 +0530

    Add gsimplecal

--- config/.config/dwm/autostart.sh
+++ config/.config/dwm/autostart.sh
@@ -4,10 +4,11 @@
 	source $HOME/.colors
 	feh --bg-scale $HOME/wall.jpg
 	slstatus &
-    pasystray &
+	pasystray &
 	nm-applet &
 	blueman-applet &
 	dunst --startup-notification true &
 	albert &
 	copyq &
+	gsimplecal &
 ) 2>&1 >/tmp/dwm.log
