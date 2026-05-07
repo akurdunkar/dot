0cb876d7794d6a3f5505696672cab2d6adb8829c
aditya-k2 <adityakurdunkar2@gmail.com>
2026-05-07 10:27:10 +0530

    Spawn displayd agent on dwm start

--- home/.config/dwm/autostart.sh
+++ home/.config/dwm/autostart.sh
@@ -11,4 +11,8 @@
 	albert &
 	copyq &
 	gsimplecal &
+	if [ -n "$DISPLAY" ]; then
+		pgrep -xa "displayd-agent" | grep displayd-agent 2>&1 >/dev/null || (nohup displayd-agent >>/tmp/displayd-agent.log 2>&1 &)
+		displayd-ctl sync
+	fi
 ) 2>&1 >/tmp/dwm.log
